997424828e010ee745a7004df880894e653dc6c5
Hasti Jalali <hastii.jalali@gmail.com>
2025-03-30 14:43:23 -0700

    add other models and fine tune

--- train.py
+++ train.py
@@ -4,17 +4,27 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
 
-def split_data(X, y, test_size=0.2, random_state=42):
+def split_data(X, y, test_size=0., random_state=42):
     return train_test_split(X, y, test_size=test_size, stratify=y, random_state=random_state)
 
-def train_models(X_train, y_train):
+
+def train_models(X_train, y_train, n_estimators=40, max_depth=4):
     logreg = LogisticRegression(penalty='l2', solver='liblinear', random_state=42)
-    rf = RandomForestClassifier(n_estimators=40, max_depth=4, class_weight='balanced', random_state=42)
-    xgb = XGBClassifier(n_estimators=50, max_depth=3, learning_rate=0.4, eval_metric='logloss', random_state=42)
+    rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, class_weight='balanced', random_state=42)
+    xgb = XGBClassifier(n_estimators=n_estimators, max_depth=max_depth, learning_rate=0.4, eval_metric='logloss', random_state=42)
+    dt = DecisionTreeClassifier(max_depth=max_depth, random_state=42)
+    nb = GaussianNB()
 
-    logreg.fit(X_train, y_train)
-    rf.fit(X_train, y_train)
-    xgb.fit(X_train, y_train)
+    models = {
+        'LogisticRegression': logreg.fit(X_train, y_train),
+        'RandomForest': rf.fit(X_train, y_train),
+        'XGBoost': xgb.fit(X_train, y_train),
+        'DecisionTree': dt.fit(X_train, y_train),
+        'NaiveBayes': nb.fit(X_train, y_train)
+    }
 
-    return {'LogisticRegression': logreg, 'RandomForest': rf, 'XGBoost': xgb}
+    return models
+
